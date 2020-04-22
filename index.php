0836977dce480c63a7656ad4566e52fe30819e48
ayman072 <ayman072@hotmail.fr>
2020-04-22 09:55:57 +0200

    salut

---
+++ index.php
@@ -0,0 +1,7 @@
+<div>
+    <label for="profile_pic">Sélectionnez le fichier à utiliser</label>
+    <input type="file" id="profile_pic" name="profile_pic" accept=".jpg, .jpeg, .png, .bmp, .svg">
+
+    <label for="name">Name (4 to 8 characters):</label>
+    <input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10">
+</div>
