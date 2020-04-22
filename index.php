10886c4ab1be4d068ea604d8da460b4e83072267
LehouxKevin <kevin.lehoux5@hotmail.fr>
2020-04-22 15:13:45 +0200

    rect selector in progress

--- index.php
+++ index.php
@@ -28,10 +28,17 @@
         </div>
         </div>
 
-        <div id="fenetre_d_affichage"><img id="affichage_image" src="#" alt="Votre image" /></div>
+        <div id="divRectangleSelection1" hidden></div>
+        <div id="divRectangleSelection2" hidden></div>
+        <div id="fenetre_d_affichage">
+            <img id="affichage_image" class='imgAfficher' src="#" alt="Votre image" />
+        </div>
 
-        <input class="input_balise" type="button" value="Début">
-        
+        <div id='buttons_containers'>
+            <input class="input_balise" type="button" value="Début">
+            <input class="input_balise" type="button" value="Fin">
+        </div>
+
         <div id="display_bar"></div>
     </body>
 </html>
--- public/js/index.js
+++ public/js/index.js
@@ -1,24 +1,65 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $("#fileToUpload").change(function() {
+var isOnDiv = false;
+
+$(document).ready(function () {
+    console.log("ready!");
+    $("#fileToUpload").change(function () {
         readURL(this);
     });
+
+    
+    $("#fenetre_d_affichage").mouseenter(function () { isOnDiv = true; console.log(isOnDiv); });
+    $("#fenetre_d_affichage").mouseleave(function () { isOnDiv = false; console.log(isOnDiv) });
+
+    setMouseListeningForRectSelector();
 });
+
+function setMouseListeningForRectSelector(isMouseOnRectDisplay) {
+    console.log("Mouse Pos : "+isMouseOnRectDisplay);
+    if (isMouseOnRectDisplay) {
+        var div = document.getElementById('divRectangleSelection1'), x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+        function reCalc() { //This will restyle the div
+            var x3 = Math.min(x1, x2); //Smaller X
+            var x4 = Math.max(x1, x2); //Larger X
+            var y3 = Math.min(y1, y2); //Smaller Y
+            var y4 = Math.max(y1, y2); //Larger Y
+            div.style.left = x3 + 'px';
+            div.style.top = y3 + 'px';
+            div.style.width = x4 - x3 + 'px';
+            div.style.height = y4 - y3 + 'px';
+        }
+        onmousedown = function (e) {
+            div.hidden = 0; //Unhide the div
+            x1 = e.clientX; //Set the initial X
+            y1 = e.clientY; //Set the initial Y
+            reCalc();
+        };
+        onmousemove = function (e) {
+            x2 = e.clientX; //Update the current position X
+            y2 = e.clientY; //Update the current position Y
+            reCalc();
+        };
+        onmouseup = function (e) {
+            div.hidden = 1; //Hide the div
+        };
+    }
+}
 
 function readURL(input) {
     if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      
-      reader.onload = function(e) {
-        $('#affichage_image').attr('src', e.target.result);
-      }
-      
-      reader.readAsDataURL(input.files[0]); // convert to base64 string
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#affichage_image').attr('src', e.target.result);
+            $('#affichage_image').attr('width', '700px');
+            $('#affichage_image').attr('height', 'auto');
+        }
+
+        reader.readAsDataURL(input.files[0]); // convert to base64 string
     }
-  }
+}
 
 
-function saveImg(ele){
+function saveImg(ele) {
     var name = $(ele).parent().find("#name").val();
 
     var formData = new FormData();
@@ -31,13 +72,13 @@ function saveImg(ele){
     $.ajax({
         type: "POST",
         url: "../../php/save.php",
-        data : formData,
+        data: formData,
         contentType: false, // NEEDED, DON'T OMIT THIS (requires jQuery 1.6+)
         processData: false, // NEEDED, DON'T OMIT THIS
-        success : function(data){
+        success: function (data) {
             console.log(data);
         },
-        error : function(){
+        error: function () {
         }
     });
 }
