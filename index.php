4951bc5fa51297c33d9c8545f1ff59d510fa4c18
ayman072 <ayman072@hotmail.fr>
2020-04-22 10:05:47 +0200

    index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <div>
     <label for="profile_pic">Sélectionnez le fichier à utiliser</label>
     <input type="file" id="profile_pic" name="profile_pic" accept=".jpg, .jpeg, .png, .bmp, .svg">
+</div>
 
-    <label for="name">Name (4 to 8 characters):</label>
-    <input type="text" id="name" name="name" required minlength="4" maxlength="8" size="10">
+<br>
+
+<div>
+    <label for="name">Name :</label>
+    <input type="text" id="name" name="name">
+
+    <input type="button" value="sauvegarder">
 </div>
--- php/save.php
+++ php/save.php
@@ -3,35 +3,15 @@
 if (isset($_FILES['fileToUpload']['name'])) {
     $info = pathinfo($_FILES['fileToUpload']['name']);
     $ext = $info['extension']; // get the extension of the file
-    if($ext != "jpg"){
-        $true = 0;
-        $result = "Erreur : Image obligatoirement en PNG";
-    }elseif($ext != "png"){
-        $true = 0;
-        $result = "Erreur : Image obligatoirement en PNG";
-    }else{
-        $target = '../Images/Cadeaux/'.$id.".".$ext;
-    }
+    $target = '../images/'.$nom.".".$ext;
 }else{
     $true = 0;
     $result = "Erreur : Image vide";
-}
-        
+}      
         
 if ($true == 1) {           
     if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target)){
         $result= "Image telechargé avec succes";
-        $dbcontroller = new DBController();
-        $stmt1 = mysqli_prepare($dbcontroller->getConn(), "UPDATE cadeaux SET libelle = ?, a = ?, b = ?, c = ? WHERE id = ?");
-        mysqli_stmt_bind_param($stmt1, 'ssssi', $libelle, $A, $B, $C, $id);
-        $data1 = $dbcontroller->executeQueryMSQL($stmt1);
-        $dbcontroller->closeQuerySQL(); 
-        if ($data1) {
-            $result .= " Opération effectuée avec succès";
-        }
-        else {
-            $result .= " Opération non effectuée";
-        }
     }else{
         $result= "Erreur : Echec telechargement de l'image";
     }
